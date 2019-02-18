ec48725715897926909baefc39f68e70ff80c5b0
yulbod <yulinabod@gmail.com>
2019-02-19 00:57:33 +0200

    Initialize repository

---
+++ Homework1/src/com/homework/Main.java
@@ -0,0 +1,13 @@
+package com.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("****   ****     *      *    *\n" +
+                           "*      *  *    * *     **  **\n" +
+                           "****   ****   *   *    * ** *\n" +
+                           "*      *     *******   *    *\n"  +
+                           "****   *    *       *  *    *");
+    }
+
+}
