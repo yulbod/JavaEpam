f9509ea0bff9a34fc6d2624a3ad255afd45c0b6d
yulbod <yulinabod@gmail.com>
2019-05-27 18:07:27 +0300

    new

---
+++ Laba_task_2_java8/src/task1/Main.java
@@ -0,0 +1,17 @@
+package task1;
+
+
+public class Main {
+    public static final int SIZE = 10;
+
+    public static void main(String[] args) {
+
+        Integer[] array = new Integer[SIZE];
+
+
+    }
+
+
+
+
+}
